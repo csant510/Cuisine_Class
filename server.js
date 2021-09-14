3fd4c9f4241fe5a07ee5f8f6dcec3c8857b321d5
csant510 <james.sant83@gmail.com>
2021-09-13 17:50:39 -0700

    touched code added layous to the code also added , added error handling and 404 message.

--- server.js
+++ server.js
@@ -1,33 +1,31 @@
 const express = require("express"),
-    app = express();
-//controlers
-homeController = require("./controllers/homeController");
-layouts = require("express-ejs-layouts");
+  app = express(),
+  homeController = require("./controllers/homeController"),
+  errorController = require("./controllers/errorController"),
+  layouts = require("express-ejs-layouts");
 
 app.set("view engine", "ejs");
 app.set("port", process.env.PORT || 3000);
 app.use(
-    express.urlencoded({
-        extended: false,
-    })
+  express.urlencoded({
+    extended: false
+  })
 );
-
-app.use(express.json()); 
-//app.use(layouts);
+app.use(express.json());
+app.use(layouts);
 app.use(express.static("public"));
 
-
-//routes
-app.get("/", (req,res) =>{
-        res.send("Welcome to Cusine Class!");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.get("/courses", homeController.showCourses);
 app.get("/contact", homeController.showSignUp);
 app.post("/contact", homeController.postedSignUpForm);
 
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
 
-
-app.listen(app.get("port"), ()=> {
-    console.log(`Sever running at http://localhost:${app.get("port")}`);
+app.listen(app.get("port"), () => {
+  console.log(`Server running at http://localhost:${app.get("port")}`);
 });
