6d675cb24bbb124dc8313e3f846bcabd75f82d12
csant510 <james.sant83@gmail.com>
2021-10-20 18:26:24 -0700

    adding virutal hook to check if user exist

--- models/course.js
+++ models/course.js
@@ -40,7 +40,32 @@ const mongoose = require("mongoose"),
     },
     {
       timestamps: true
-    }
-  );
+    });
+
+    userSchema.virtual("fullName").get(function() {
+        return `${this.name.first} ${this.name.last}`;
+      });
+      
+      userSchema.pre("save", function (next) {
+        let user = this;
+        if (user.subscribedAccount === undefined) {
+          Subscriber.findOne({
+            email: user.email
+          })
+            .then(subscriber => {
+              user.subscribedAccount = subscriber;
+              next();
+            })
+            .catch(error => {
+              console.log(`Error in connecting subscriber:
+       ${error.message}`);
+              next(error);
+            });
+        } else {
+          next();
+        }
+      });
+      
+      
 module.exports = mongoose.model("User", userSchema);
 
