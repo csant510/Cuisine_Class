dad268bf30a662a6bcd0bde64109b6059bc1b058
csant510 <james.sant83@gmail.com>
2021-11-07 19:35:34 -0800

    added modal, api routes, json, ajax request linked the pages

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = require("express").Router(),
   subscriberRoutes = require("./subscriberRoutes"),
   courseRoutes = require("./courseRoutes"),
   errorRoutes = require("./errorRoutes"),
-  homeRoutes = require("./homeRoutes");
+  homeRoutes = require("./homeRoutes"),
+  apiRoutes = require("./apiRoutes");
 
+router.use("/api", apiRoutes);
 router.use("/users", userRoutes);
 router.use("/subscribers", subscriberRoutes);
 router.use("/courses", courseRoutes);
@@ -12,4 +14,3 @@ router.use("/", homeRoutes);
 router.use("/", errorRoutes);
 
 module.exports = router;
-
