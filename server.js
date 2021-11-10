1ac319758ca60161222b2b42c9022ae261d9f246
csant510 <james.sant83@gmail.com>
2021-11-10 15:27:41 -0800

    added socket reuqire socket io to to the server.js file I alos added the controller and commit it out next I will create the chat controller

--- server.js
+++ server.js
@@ -70,7 +70,8 @@ app.use((req, res, next) => {
 });
 
 app.use("/", router);
-
-app.listen(app.get("port"), () => {
+const server = app.listen(app.get("port"), () => {
   console.log(`Server running at http://localhost:${app.get("port")}`);
-});
+}),
+io = require("socket.io")(server);
+//chatController = require("./controllers/chatController")(io);
