84defe38ac17bf1b3847d68ae839140ad1462d14
csant510 <james.sant83@gmail.com>
2021-11-10 16:02:13 -0800

    update the chat controller require the models then created action to save, messages, also to know when users disconet from the chat, alos to limit the amount of messages in the chat

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,20 +1,34 @@
+const Message = require("../models/message");
+
 module.exports = io => {
-    io.on("connection", client => {
-      console.log("new connection");
-  
-      client.on("disconnect", () => {
-        console.log("user disconnected");
+  io.on("connection", client => {
+    Message.find({})
+      .sort({
+        createdAt: -1
+      })
+      .limit(10)
+      .then(messages => {
+        client.emit("load all messages", messages.reverse());
       });
-  
-      client.on("message", (data) => {
-        let messageAttributes = {
+
+    console.log("new connection");
+
+    client.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+
+    client.on("message", data => {
+      let messageAttributes = {
           content: data.content,
           userName: data.userName,
           user: data.userId
-        };
-        io.emit("message");
-      });
+        },
+        m = new Message(messageAttributes);
+      m.save()
+        .then(() => {
+          io.emit("message", messageAttributes);
+        })
+        .catch(error => console.log(`error: ${error.message}`));
     });
-  };
-  
-  
+  });
+};
